24eca5bf91751dffc4e89445e03accc5a90b745d
@jadiazp <jadiazp@indracompany.com>
2023-01-14 12:58:34 -0500

    fix add HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate, HashRouter } from "react-router-dom";
 import { Navbar } from "@/widgets/layout";
 import routes from "@/routes";
 
 function App() {
   return (
-    <>
+    <HashRouter>
       <div className="container absolute left-2/4 z-10 mx-auto -translate-x-2/4 p-4">
         <Navbar routes={routes} />
       </div>
@@ -16,7 +16,7 @@ function App() {
         <Route path="*" element={<Navigate to="/home" replace />} />
         <Route path="/sign-in" element={<Navigate to="/sign-in" replace />} />
       </Routes>
-    </>
+    </HashRouter>
   );
 }
 
